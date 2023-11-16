e3d8381d3101ff90522935493d3f1dee1ed82152
ErnestLopez <al350346@edu.uaa.mx>
2023-11-16 07:20:38 -0600

    Terminando buddy system

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-
-/* 
-Equipo 1 conformado por:
-Miguel Ángel Batres Luna     ID: 350553 
-Juan Damián Ortega De Luna   ID: 228387
-Luis Ernesto López Cárdenas  ID: 350346 
-Ariel Emilio Parra Martínez  ID: 280862 
-*/
-
-//Probando
-
 #include <iostream>
 #include <vector>
 #include <time.h>
@@ -16,31 +5,12 @@ Ariel Emilio Parra Martínez  ID: 280862
 using namespace std;
 
 //Esto representara un bloque de memoria en BUDDY SYSTEM
-struct BLOQUE_DE_MEMORIA{
+struct BLOQUE_DE_MEMORIA {
     int tamaño;
     bool libre;
+    BLOQUE_DE_MEMORIA* izq;
+    BLOQUE_DE_MEMORIA* der;
 };
-//Esto vendria siendo una estructura donde implementemos un arbol en la memoria
-struct NodoMemoria {
-    int tamanio;
-    bool libre;
-    NodoMemoria* left;
-    NodoMemoria* right;
-};
-
-//Crear la memoriaTipo arbol usando la estructura de arbol
-NodoMemoria* CreaArbol(int tamanio) {
-    NodoMemoria* nodo = new NodoMemoria;
-    nodo->tamanio = tamanio;
-    nodo->libre = true;
-    nodo->left = NULL;
-    nodo->right = NULL;
-    return nodo;
-}
-
-//Agregar una funcion que coloque el proceso en un nodo con su tamaño
-
-
 
 //Esto representara un proceso
 struct PROCESO{
@@ -53,16 +23,22 @@ struct PROCESO{
 
 //Esto representara la cola de procesos
 vector<PROCESO> Cola_procesos;
+BLOQUE_DE_MEMORIA* MEMORIA;
 
+const int TAMAÑO_MEMORIA = 1024; // Tamaño total de la memoria
+
+void inicializarMemoria() {
+    MEMORIA = new BLOQUE_DE_MEMORIA{ TAMAÑO_MEMORIA, true, nullptr, nullptr };
+}
 //Representara la memoria, cabe destacar, que la memoria NO ES DINAMICA
 //La memoria TIENE TAMAÑO FIJO, pero se dividira en segmentos
-vector<BLOQUE_DE_MEMORIA> MEMORIA;
+//vector<BLOQUE_DE_MEMORIA> MEMORIA;
 
 //TAMAÑO DEL QUANTUM DEL SISTEMA 
 //Esto representa el quantum que se le restara a los procesos equitativamente 
 //Es decir A TODOS
 const int QUANTUM_SYSTEM = 2;
-
+ 
 
 // LAS FUNCIONES DE BUDDY SYSTEM
 // HABRAN DE MOMENTO 2, una sera dividir la memoria cuando un proceso se vaya a 
@@ -77,16 +53,54 @@ const int QUANTUM_SYSTEM = 2;
 
 
 // DIVIDIR MEMORIA
-BLOQUE_DE_MEMORIA* dividirmemoria(int TAMAÑO_PEDIDO){
+/*Esta funcion se encarga de dividir un bloque de memoria con Buddy System. Recibe un puntero al bloque actual bloque y el tamaño requerido TAMAÑO_PEDIDO
+Luego, verifica si el bloque es lo suficientemente grande para dividirse en dos
+Si si es, crea dos bloques hijos (izquierda y derecha) y actualiza el bloque actual
+Luego, decide en cual de los bloques hijos asignar el espacio llamando recursivamente a dividirmemoria.
+Si el bloque no es lo suficientemente grande, simplemente marca el bloque como no libre y lo devuelve*/
 
-    //rifense
-    return nullptr;
+BLOQUE_DE_MEMORIA* dividirmemoria(BLOQUE_DE_MEMORIA* bloque, int TAMAÑO_PEDIDO) {
+    if (bloque->tamaño >= TAMAÑO_PEDIDO * 2) {
+        // Crear dos bloques hijos
+        bloque->izq = new BLOQUE_DE_MEMORIA{ bloque->tamaño / 2, true, nullptr, nullptr };
+        bloque->der = new BLOQUE_DE_MEMORIA{ bloque->tamaño / 2, true, nullptr, nullptr };
+
+        // Actualizar el bloque actual
+        bloque->tamaño /= 2;
+        bloque->libre = false;
+
+        // Es para decidir en cual de los bloques hijos se va a asignar la memoria
+        if (TAMAÑO_PEDIDO <= bloque->izq->tamaño) {
+            return dividirmemoria(bloque->izq, TAMAÑO_PEDIDO);
+        } else {
+            return dividirmemoria(bloque->der, TAMAÑO_PEDIDO);
+        }
+    } else {
+        bloque->libre = false;
+        return bloque;
+    }
 }
 
 // LIBERAR MEMORIA
-void liberarmemoria(BLOQUE_DE_MEMORIA* bloque_de_memoria){
+/*Esta funcion libera un bloque de memoria y fusiona bloques juntos que estan libres
+Recibe un puntero al bloque que se va a liberar, luego marca el bloque como libre
+Luego, entra en un bucle que fusiona bloques libres que estan juntos. Si ambos bloques hijos estan libres, fusiona el bloque actual con su hermano, duplica su tamaño 
+y mueve el puntero al bloque padre
+El bucle continua hasta que no se pueden fusionar mas bloques o hasta llegar a la raiz del arbol */
+void liberarmemoria(BLOQUE_DE_MEMORIA* bloque) {
+    bloque->libre = true;
 
-   //rifense
+    // Fusionar bloques libres que estan juntos
+    while (bloque->izq != nullptr && bloque->der != nullptr &&
+           bloque->izq->libre && bloque->der->libre) {
+        // Fusionar con el bloque hermano
+        bloque->izq = nullptr;
+        bloque->der = nullptr;
+        bloque->tamaño *= 2;
+
+        // Mover al bloque padre
+        bloque = bloque->izq;
+    }
 }
 
 // ESTE ES EL PLANIFICADOR ROUND ROBIN
@@ -168,10 +182,10 @@ void imprimir_memoria(){
 
 int main(){
    srand(time(NULL));  
+    //Generamos la memoria
+   inicializarMemoria();
+
    //El id de los procesos
-   MEMORIA.push_back({ 1024, true }); //al vector memoria, le agregamos las datos tamanio y libre
-   //Se inicia el arbol con izquierda y derecha en NULL
-   NodoMemoria* MemoriaArbol = CreaArbol(1024);
    int id_procesos;
 
    //Aqui un for rapidito donde genero 20 procesos aleatoriamente

3b6e7781b0740a1acfcc5b9e0b5cca2e48180b03
maleficarius <maesito23@hotmail.com>
2023-11-23 08:43:40 -0600

    cambios chidos en rama de ernesto

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include <cstdlib>
 #include <unistd.h>
 using namespace std;
+/*Funcinoes Auxiliares*/
+#if defined(_WIN32)
+void clear() { system("cls"); }
+#else
+void clear() { system("clear"); }
+#endif
 
 //Esto representara un bloque de memoria en BUDDY SYSTEM
 struct BLOQUE_DE_MEMORIA {
@@ -28,7 +34,6 @@ vector<PROCESO> Cola_procesos;
 BLOQUE_DE_MEMORIA* MEMORIA;
 vector<int> memoria;
 vector<PROCESO> Cola_lista;
-
 
 const int TAMAÑO_MEMORIA = 1024; // Tamaño total de la memoria
 
@@ -64,34 +69,42 @@ Si si es, crea dos bloques hijos (izquierda y derecha) y actualiza el bloque act
 Luego, decide en cual de los bloques hijos asignar el espacio llamando recursivamente a dividirmemoria.
 Si el bloque no es lo suficientemente grande, simplemente marca el bloque como no libre y lo devuelve*/
 
-bool sepuededividir(BLOQUE_DE_MEMORIA* bloque, PROCESO* actual, int tamaño) {
+bool sepuededividir(BLOQUE_DE_MEMORIA* bloque, PROCESO* actual) {
     if (bloque->libre) {
-        if (tamaño <= (bloque->tamaño / 2)) {
+        if ((actual->tamaño) <= (bloque->tamaño / 2)) {
             if (bloque->izq == nullptr) {
-                bloque->izq = new BLOQUE_DE_MEMORIA{ tamaño / 2, true, 0, nullptr, nullptr };
+                bloque->izq = new BLOQUE_DE_MEMORIA{ (bloque->tamaño / 2), true, 0, nullptr, nullptr };
             }
             if (bloque->der == nullptr) {
-                bloque->der = new BLOQUE_DE_MEMORIA{ tamaño / 2, true, 0, nullptr, nullptr };
+                bloque->der = new BLOQUE_DE_MEMORIA{ (bloque->tamaño / 2), true, 0, nullptr, nullptr };
             }
-            if (sepuededividir(bloque->izq, actual, tamaño) || sepuededividir(bloque->der, actual, tamaño)) {
+            if (sepuededividir(bloque->izq,actual)){
+                return true;
+            }
+            else if(sepuededividir(bloque->der, actual)) {
                 return true;
             } else {
                 return false;
             }
-        } else {
+        } else{
+            if(bloque->tamaño > actual->tamaño){
             bloque->libre = false;
             bloque->idprocesoqueocupa = actual->idproceso;
             return true;
+            }else{
+                return false;
+            }
         }
     } else {
-        if (bloque->izq != nullptr && sepuededividir(bloque->izq, actual, tamaño)) {
+        if (bloque->izq != nullptr && sepuededividir(bloque->izq, actual)){
             return true;
-        } else if (bloque->der != nullptr && sepuededividir(bloque->der, actual, tamaño)) {
+        } else if (bloque->der != nullptr && sepuededividir(bloque->der, actual)){
             return true;
         } else {
             return false;
         }
     }
+
 }
 
 
@@ -102,29 +115,36 @@ Luego, entra en un bucle que fusiona bloques libres que estan juntos. Si ambos b
 y mueve el puntero al bloque padre
 El bucle continua hasta que no se pueden fusionar mas bloques o hasta llegar a la raiz del arbol */
 void liberarmemoria(BLOQUE_DE_MEMORIA* bloque, PROCESO actual) {
-    
-    if(bloque->izq->idprocesoqueocupa == actual.idproceso){
-        bloque->izq->libre = true;
-        if(bloque->der->libre == true){
-            bloque->izq = nullptr;
-            bloque->der = nullptr;
-            bloque->tamaño *= 2;
+    if (bloque != nullptr) {
+        if (bloque->idprocesoqueocupa == actual.idproceso) {
+            // Liberar el bloque actual
+            bloque->libre = true;
+            bloque->idprocesoqueocupa = -1;
+
+            // Intentar fusionar con el bloque hermano
+            if (bloque->izq != nullptr && bloque->izq->libre && bloque->der != nullptr && bloque->der->libre) {
+                // Ambos bloques adyacentes son libres, fusionarlos
+                bloque->izq = nullptr;
+                bloque->der = nullptr;
+                liberarmemoria(bloque, actual);  // Intentar fusionar hacia arriba
+            }
+        } else {
+            // Liberar en los bloques hijos si están libres
+            if (bloque->izq != nullptr && bloque->izq->libre) {
+                bloque->izq = nullptr;
+            }
+            liberarmemoria(bloque->izq, actual);
+
+            if (bloque->der != nullptr && bloque->der->libre) {
+                bloque->der = nullptr;
+            }
+            liberarmemoria(bloque->der, actual);
         }
-    }else if(bloque->der->idprocesoqueocupa == actual.idproceso){
-        bloque->der->libre = true;
-        if(bloque->izq->libre == true){
-            bloque->izq = nullptr;
-            bloque->der = nullptr;
-            bloque->tamaño *= 2;
-        }
-    }else if(bloque->izq!=nullptr && bloque->der!=nullptr){
-        liberarmemoria(bloque->izq, actual);
-        liberarmemoria(bloque->der, actual);
     }
 }
+
+
 void imprimir_memoria(){
-   bool imprimir;
-   int i,j;
    cout << endl;
    for (int i = 1; i <= 96; ++i) {
     bool imprimir = true; 
@@ -170,7 +190,7 @@ PROCESO generar_proceso(int& id_procesos){
 
     //El tamaño limite de tamaño del proceso
     int TAMAÑOmin = 1;
-    int TAMAÑOmax = 1024;
+    int TAMAÑOmax = 600;
     //El tamaño limite del cuantum del proceso
     int CUANTUMmin = 1;
     int CUANTUMmax = 10;
@@ -190,63 +210,66 @@ PROCESO generar_proceso(int& id_procesos){
 }
 // ESTE ES EL PLANIFICADOR ROUND ROBIN
 void PLANIFICADOR(){
-    int id_procesos = 0;
-    for(int i=0;i<Cola_lista.size();i++){
+    int i=0;
+    while(true){
 
-        system("clear");
-        memoria.clear();
-        llenarvector(MEMORIA,96,true,false);
-        imprimir_memoria();
+        clear();
         imprimir_procesos();
+        //llenarvector(MEMORIA,96,true,false);
+        //imprimir_memoria();
 
-        //Aqui inicializo el proceso actual, que sera el que esta al frente de la cola  
-
-        //aqui deberia de ir un if para comprobar si hay espacio en memoria
-        //esta seria la implementacion de buddy system en round robin :)
-       // if(Proceso_esperando == false){
         PROCESO PorEntrar = Cola_lista[i];
 
-        bool sepuede = sepuededividir(MEMORIA,&PorEntrar,PorEntrar.tamaño);
-        
+        bool sepuede = sepuededividir(MEMORIA,&PorEntrar);
+        cout << "Proceso por entrar: "<<"("<<PorEntrar.idproceso<<","<<PorEntrar.tamaño<<","<<PorEntrar.quantumproceso<<")"<<endl;
+        sleep(2);
         if(sepuede){
-        
-        Cola_procesos.push_back(PorEntrar);
-        PROCESO begin = Cola_procesos.front();
-        
-        begin.quantumproceso = begin.quantumproceso - QUANTUM_SYSTEM;
-        
-        //Si el proceso es mayor que 0 quiere decir que todavia no ha acabado
-        if(begin.quantumproceso <= 0){
-            Cola_procesos.erase(Cola_procesos.begin());
-            liberarmemoria(MEMORIA,begin);
-        }else{
-        
-        //Elimino ese proceso usando su iterador
-        
-        Cola_procesos.erase(Cola_procesos.begin());
+            Cola_procesos.push_back(PorEntrar);
+            PROCESO begin = Cola_procesos.front();
+            clear();
+            imprimir_procesos();
+            cout << "Proceso a ejecutar: "<<"("<<begin.idproceso<<","<<begin.tamaño<<","<<begin.quantumproceso<<")"<<endl;
+            sleep(2);
 
-        //ESTA LINEA REPRESENTA QUE EL PROCESO SE ESTA EJECUTANDO
-        Cola_procesos.push_back(begin);
-        }
+            begin.quantumproceso = begin.quantumproceso - QUANTUM_SYSTEM;
+        
+            //Si el proceso es mayor que 0 quiere decir que todavia no ha acabado
+            if(begin.quantumproceso <= 0){
+                Cola_procesos.erase(Cola_procesos.begin());
+                liberarmemoria(MEMORIA,begin);
+            }else{               
+                Cola_procesos.erase(Cola_procesos.begin());
+                //ESTA LINEA REPRESENTA QUE EL PROCESO SE ESTA EJECUTANDO
+                Cola_procesos.push_back(begin);   
+            }
+            clear();
+            imprimir_procesos();
+            sleep(2);
+            i+=1;
 
         }else{
-              
-              i-=1;
-              //Declaro el proceso del inicio
+
+              cout << "Proceso por entrar: "<<"("<<PorEntrar.idproceso<<","<<PorEntrar.tamaño<<","<<PorEntrar.quantumproceso<<")"<<endl;
               PROCESO begin = Cola_procesos.front();
-              //EJECUTAR
+              clear();
+              imprimir_procesos();
+              cout << "Proceso a ejecutar: "<<"("<<begin.idproceso<<","<<begin.tamaño<<","<<begin.quantumproceso<<")"<<endl;
+              sleep(2);
               begin.quantumproceso = begin.quantumproceso - QUANTUM_SYSTEM;
+
               if(begin.quantumproceso <= 0){
                   Cola_procesos.erase(Cola_procesos.begin());
                   liberarmemoria(MEMORIA,begin);
               }else{
+
               Cola_procesos.erase(Cola_procesos.begin());
               //ESTA LINEA REPRESENTA QUE EL PROCESO SE ESTA EJECUTANDO
               Cola_procesos.push_back(begin);
               }
-        
+              clear();
+              imprimir_procesos();
+              sleep(2);
         }
-        sleep(2);
     }
 }
 
